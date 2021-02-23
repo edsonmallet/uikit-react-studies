b698679a468e7d589dc018be07613eb89c04c778
Edson Mallet <edsonmmr@gmail.com>
2021-02-23 14:31:47 -0300

    fix: import and export in index

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
-export { Button } from "./components/Button";
-export { Input } from "./components/Input";
-export { Radio } from "./components/Radio";
-export { CheckBox } from "./components/Checkbox";
-export { Modal } from "./components/Modal";
+import { Button } from "./components/Button";
+import { Input } from "./components/Input";
+import { Radio } from "./components/Radio";
+import { CheckBox } from "./components/Checkbox";
+import { Modal } from "./components/Modal";
+
+export { Button, Input, Radio, CheckBox, Modal };
